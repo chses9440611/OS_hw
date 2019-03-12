89f0059a8ea032660dea7cc5a1c4259417514486
chses910372 <chses910372@gmail.com>
2019-03-12 20:24:17 +0800

    Complete hw2-2

--- oshw/hw2/hw2-2/process_2.cpp
+++ oshw/hw2/hw2-2/process_2.cpp
@@ -9,8 +9,27 @@ int main(int argc ,char* argv[]){
     printf("process %d create process %d\n",getpid(),pidx=fork());
     printf("process %d create process %d\n",getpid(),pidy=fork());
     printf("process %d create process %d\n",getpid(),pidz=fork());
-    /*
-        your code
-    */
+
+
+	if(pidz == 0){
+		pidx = 0;
+		pidy = 0;
+	}
+	else if(pidy == 0){
+		waitpid(pidz, NULL, 0);
+		pidx = 0;
+	}
+	else if(pidx == 0) {
+		waitpid(pidy, NULL, 0);
+		waitpid(pidz, NULL, 0);
+	}
+	else{
+		waitpid(pidx, NULL, 0);
+		waitpid(pidy, NULL, 0);
+		waitpid(pidz, NULL, 0);
+
+	}
+
+
     printf("process %d deaded its child process %d %d %d\n",getpid(),pidx,pidy,pidz);
 }
