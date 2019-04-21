f4b1c0355623507ca706f7a6c37fe68b3467ec2f
chses910372 <chses910372@gmail.com>
2019-04-21 08:19:30 +0800

    Complete the hw3.py

--- oshw/hw3/hw3.py
+++ oshw/hw3/hw3.py
@@ -3,22 +3,19 @@
 import random
 import string
 import hashlib
-from socketserver import TCPServer
 from threading import Thread
+from socketserver import BaseRequestHandler, TCPServer
 
 def server_work(message, pow_diff=3):
     """genetrate POW string"""
-    pow_check = pow_diff * '0'
-    seed = id_generator()
-    hash_value = create_hashvalue(seed+message)
-    while hash_value[:pow_diff] > pow_check:
+    pow_check = pow_diff * '0' # decide the # of 0
+    seed = id_generator() # create the seed
+    hash_value = create_hashvalue(seed+message) # create the hash value
+    while hash_value[:pow_diff] > pow_check: # while create failure, redo
         seed = id_generator()
         hash_value = create_hashvalue(seed+message)
 
-    print(hash_value)
-    print(hash_value[:3])
     return message + ',' + seed + ',' + hash_value + '\n'
-
 
 def id_generator(size=5, chars=string.ascii_letters + string.digits):
     """Generating seed"""
@@ -28,15 +25,37 @@ def id_generator(size=5, chars=string.ascii_letters + string.digits):
 
     return value
 
-
 def create_hashvalue(message):
     """Create hash value"""
     hash_obj = hashlib.sha256()
-    hash_obj.update(message.encode("utf-8"))
+    hash_obj.update(message.encode("utf-8")) # change message from string to bytes
     return hash_obj.hexdigest()
 
+
+class EchoHandler(BaseRequestHandler):
+    """Process request"""
+    def handle(self):
+        """ The function to recv and send result"""
+        print('Got connection from: ', self.client_address)
+        while True:
+            msg = self.request.recv(8192)
+            if not msg:
+                break
+            msg = (msg.decode()).replace('\n', '') #change message from bytes to string and remove '\n'
+            #print("Message: ", msg)
+            msg2 = server_work(msg)
+            self.request.send(msg2.encode()) # change msg2 from string to bytes
+
 if __name__ == '__main__':
-    print(server_work('70NVO'))
-
-
+    try:
+        NUMTHREADING = 100
+        SERV = TCPServer(('', 20000), EchoHandler)
+        for n in range(NUMTHREADING):
+            tSocket = Thread(target=SERV.serve_forever)
+            tSocket.daemon = True
+            tSocket.start()
+        SERV.serve_forever()
+    except KeyboardInterrupt:
+            print("ByeBye!")
+            SERV.server_close()
 
