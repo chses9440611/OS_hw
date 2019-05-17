85adbeae5cbc0fb95bb1293d4d8335b59084e43c
chses910372 <chses910372@gmail.com>
2019-05-17 17:41:37 +0800

    spinlock w/o context switch

--- oshw/hw4/spinlock/spinlock.s
+++ oshw/hw4/spinlock/spinlock.s
@@ -4,8 +4,20 @@
 .global spin_unlock
 
 spin_lock:
-	
+	pushq %rbp
+	movq %rsp, %rbp
+	movq %rdi, -8(%rbp)
+L1:
+	cmp $0, -8(%rbp)
+	je L1
+	movl $0, -8(%rbp)
+
+	pop %rbp
 	ret
 
 spin_unlock:
+	push %rbp
+	mov %rsp, %rbp
+	movl $1, -8(%rbp)
+	pop %rbp
 	ret
